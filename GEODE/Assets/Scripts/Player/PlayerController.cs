514cb707e2de830c2ef61a447d21405b70a5a5dc
QuincyStokes <quincy.f.stokes@gmail.com>
2025-03-02 07:50:00 -0800

    player animation

--- GEODE/Assets/Scripts/Player/PlayerController.cs
+++ GEODE/Assets/Scripts/Player/PlayerController.cs
@@ -11,50 +11,61 @@ public class PlayerController : NetworkBehaviour
 {
     [Header("References")]
     [SerializeField] private PlayerInventory playerInventory;
+    [SerializeField] private Animator animator;
+    private Rigidbody2D rb;
 
-    [Header("Movement Settings")]
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float knockbackDecay;
+    private Vector2 localInputVelocity;
+    private Vector2 lastMovedDir;
+    private Vector2 externalVelocity;
     
+    private NetworkVariable<Vector2> networkDirection = new NetworkVariable<Vector2>( //store our directoin as a network variable so other clients can update
+        Vector2.down, 
+        NetworkVariableReadPermission.Everyone, 
+        NetworkVariableWritePermission.Server
+    );
 
-    private Vector2 inputVelocity;
+    private NetworkVariable<float> networkVelocity = new NetworkVariable<float>( //same as networkDirection
+        0f, 
+        NetworkVariableReadPermission.Everyone, 
+        NetworkVariableWritePermission.Server
+    );
+
 
     
-    [Header("Health Settings")]
+    [Header("Health")]
     [SerializeField] private float maxHealth;
+    private float currentHealth;
 
 
     //INPUTS  -----------------------
-
+    [Header("Input")]
     [SerializeField] public PlayerInputActionMap playerInput; //this is the input action map, essentially there are a bunch of actions we can grab and assign
     private InputAction movementInput; 
     private InputAction mouseInput;
-
-    [Header("Internal Use")]
-    private Vector2 externalVelocity;
-    private float currentHealth;
     private Vector3 mousePos;
     private Vector3Int previousMousePosInt;
+
 
     //WEIRD THING, just going to create a field to the ItemDatabase so it loads.. kindof a hack but it works?
     [SerializeField]private ItemDatabase itemDatabase;
 
 
     
-    //input movement, has 
-
-    private Rigidbody2D rb;
+    //PRIVATE INTERNAL
+    
 
     public override void OnNetworkSpawn()
     {
-        if(!IsOwner)
-        {
-            enabled = false;
-        }
+        
+        networkDirection.OnValueChanged += OnNetworkDirectionChanged;
+        networkVelocity.OnValueChanged += OnNetworkVelocityChanged;
+        
     }
     private void Awake()
     {
-       
         rb = GetComponentInChildren<Rigidbody2D>();
         playerInput = new PlayerInputActionMap();
 
@@ -62,8 +73,11 @@ public class PlayerController : NetworkBehaviour
 
     private void OnEnable()
     {
+        if(!IsOwner)
+        {
+            return;
+        }
         //ENABLE CALLBACKS
-        
         //Movement
         movementInput = playerInput.Player.Move;
         movementInput.Enable();
@@ -91,6 +105,10 @@ public class PlayerController : NetworkBehaviour
 
     private void OnDisable()
     {
+        if(!IsOwner)
+        {
+            return;
+        }
         //DISABLE CALLBACKS
 
         //Movement
@@ -120,26 +138,84 @@ public class PlayerController : NetworkBehaviour
     
     private void Start()
     {
-        CameraManager.Instance.FollowPlayer(transform);
+        if(!IsOwner)
+        {
+            return;
+        }
+        
     }
 
     private void Update()
     {
+        if(!IsOwner)
+        {
+            return;
+        }
         MousePositionHandler();
     }
 
     private void FixedUpdate()
     {
+        if(!IsOwner)
+        {
+            return;
+        }
         MovementUpdate();
     }
 
     private void MovementUpdate()
     {
-        inputVelocity = movementInput.ReadValue<Vector2>().normalized * moveSpeed;
-        Vector2 finalVelocity = inputVelocity + externalVelocity;
+        if(!IsOwner)
+        {
+            return;
+        }
+        localInputVelocity = movementInput.ReadValue<Vector2>().normalized * moveSpeed;
+        Vector2 finalVelocity = localInputVelocity + externalVelocity;
         rb.linearVelocity = finalVelocity;
 
+        //animation?
+        if(finalVelocity.sqrMagnitude > 0.01f)
+        {
+            lastMovedDir = finalVelocity.normalized;
+        }
+
+        animator.SetFloat("moveX", lastMovedDir.x);
+        animator.SetFloat("moveY", lastMovedDir.y);
+        animator.SetFloat("velocity", finalVelocity.magnitude);
+
+        bool directionChanged = Vector2.Distance(networkDirection.Value, lastMovedDir) > .001f;
+        bool velocityChanged = Math.Abs(networkVelocity.Value - finalVelocity.sqrMagnitude) > .001f;
+
+        if(directionChanged || velocityChanged)
+        {
+            UpdateMovementServerRpc(lastMovedDir, finalVelocity.magnitude);
+        }
+
         externalVelocity = Vector2.Lerp(externalVelocity, Vector2.zero, knockbackDecay * Time.fixedDeltaTime);
+    }
+
+    private void OnNetworkDirectionChanged(Vector2 oldValue, Vector2 newValue)
+    {
+        if(!IsOwner)
+        {
+            animator.SetFloat("moveX", newValue.x);
+            animator.SetFloat("moveY", newValue.y);
+        }
+    }
+
+    private void OnNetworkVelocityChanged(float oldValue, float newValue)
+    {
+        if(!IsOwner)
+        {
+            animator.SetFloat("velocity", newValue);
+        }
+    }
+
+    [ServerRpc]
+    private void UpdateMovementServerRpc(Vector2 direction, float velocity)
+    {
+        networkDirection.Value = direction;
+        networkVelocity.Value = velocity;
     }
 
     //this works pretty well
@@ -164,6 +240,14 @@ public class PlayerController : NetworkBehaviour
 
     private void MousePositionHandler()
     {
+        if(!IsOwner)
+        {
+            return;
+        }
+
+        Debug.Log("Camera.main: " + Camera.main);
+        Debug.Log("mouseInput: " + mouseInput);
+
         mousePos = Camera.main.ScreenToWorldPoint(mouseInput.ReadValue<Vector2>());
         Vector3Int mousePosInt = new Vector3Int((int)mousePos.x, (int)mousePos.y, 0);
         if(mousePosInt != previousMousePosInt && GridManager.Instance != null)
