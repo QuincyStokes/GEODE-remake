ba543c8d2f0adfd5a07041df45c9889e797191ae
qstokes <quincy.f.stokes@gmail.com>
2025-06-21 17:23:29 -0700

    (Bugfixes, Feature in Progress) StructureSprites, Stone wall
    Added large stone wall sprite, reorganized 64x64 aseprite file.

--- GEODE/Assets/Scripts/Audio/BackgroundMusicManager.cs
+++ GEODE/Assets/Scripts/Audio/BackgroundMusicManager.cs
@@ -9,10 +9,11 @@ public class BackgroundMusicManager : MonoBehaviour
     private void Start()
     {
         LobbyHandler.Instance.OnGameStarted += StopMusic;
-        ConnectionManager.Instance.OnPlayerSpawned += PlayMusic;
-        DayCycleManager.Instance.becameNight += PlayNightMusic;
+        ConnectionManager.Instance.OnPlayerSpawned += PlayDayMusic;
+        DayCycleManager.Instance.becameNight += PlayNightMusic; //! error, doesn't exist when this awakens. 
+        DayCycleManager.Instance.becameDay += PlayDayMusic; //! error
         AudioManager.Instance.OnMusicEnded += PlayMusic;
-        AudioManager.Instance.PlayMusic(MusicId.Forest_Day);
+        //AudioManager.Instance.PlayMusic(MusicId.Forest_Day); Shoulnd't need this since OnPlayerSpawned does?
     }
 
     private void StopMusic()
@@ -22,12 +23,47 @@ public class BackgroundMusicManager : MonoBehaviour
 
     private void PlayMusic()
     {
-        AudioManager.Instance.PlayMusic(MusicId.Forest_Day);
+        if (DayCycleManager.Instance.IsNighttime())
+        {
+            PlayNightMusic();
+        }
+        else
+        {
+            PlayDayMusic();
+        }
     }
 
     private void PlayNightMusic()
     {
-        //AudioManager.Instance.PlayClientRpc(MusicId.Forest_Night);     <------- Eventually something like this
+        Vector3 playerPos = PlayerController.Instance.transform.position;
+        Vector3Int pos = new Vector3Int((int)playerPos.x, (int)playerPos.y, (int)playerPos.z);
+        BiomeType biomeType = WorldGenManager.Instance.GetBiomeAtPosition(pos);
+        switch (biomeType)
+        {
+            case BiomeType.Forest:
+                //AudioManager.Instance.PlayClientRpc(MusicId.Forest_Night);
+                break;
+            case BiomeType.Desert:
+                //AudioManager.Instance.PlayMusic(MusicId.Desert_Night);
+                break;
+        }
+        
+    }
+
+    private void PlayDayMusic()
+    {
+        Vector3 playerPos = PlayerController.Instance.transform.position;
+        Vector3Int pos = new Vector3Int((int)playerPos.x, (int)playerPos.y, (int)playerPos.z);
+        BiomeType biomeType = WorldGenManager.Instance.GetBiomeAtPosition(pos);
+        switch (biomeType)
+        {
+            case BiomeType.Forest:
+                AudioManager.Instance.PlayMusic(MusicId.Forest_Day);
+                break;
+            case BiomeType.Desert:
+                Debug.LogWarning("Warning. Trying to play Desert music, but aint no Desert yet.");
+                break;
+        }
     }
 
     private void OnDisable()
