10488de6f1612d31e66c977b93a34e4e13e03d0c
qstokes <quincy.f.stokes@gmail.com>
2025-12-09 12:17:41 -0800

    Click to copy lobby code

---
+++ GEODE/Assets/Scripts/UI/Main Menu/LobbyCodeCopier.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEditor.PackageManager;
+
+public class LobbyCodeCopier : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private TMP_Text lobbyCodeText;
+    [SerializeField] private GameObject tooltip;
+    [SerializeField] private TMP_Text tooltipText;
+
+    private bool isShowingTooltip = false;
+
+    private void Update()
+    {
+        if(isShowingTooltip)
+        {
+            Vector2 position;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                transform as RectTransform,
+                Input.mousePosition,
+                null,
+                out position);
+            tooltip.transform.localPosition = position + new Vector2(5, 5);
+        }   
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        GUIUtility.systemCopyBuffer = lobbyCodeText.text;
+        tooltipText.text = "Copied!";
+        LobbyErrorMessages.Instance.SetError("Lobby code copied to clipboard!", false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isShowingTooltip = true;
+        tooltip.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isShowingTooltip = false;
+        tooltip.SetActive(false);
+        tooltipText.text = "Click to copy";
+    }
+}
